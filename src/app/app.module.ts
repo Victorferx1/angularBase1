93968c40613ae275be1367612611049adacaba69
Victor Fernandez <victorferx@outlook.com>
2023-03-29 10:27:12 -0500

    registro de appDBZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.Module';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HeroesModule } from './heroes/heroes.Module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
